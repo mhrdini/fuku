bccd9285b5c643ad9f76fba29f6022850a5e655b
Dini Putri <mahardini.rizky@gmail.com>
2025-11-01 12:25:52 +0900

    perf: use correct trpc imperatives for server- and client-components

--- packages/api/src/routers/app.ts
+++ packages/api/src/routers/app.ts
@@ -1,8 +1,10 @@
 import { authRouter } from '../routers/auth'
 import { createTRPCRouter } from '../trpc'
+import { userRouter } from './user'
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  user: userRouter,
 })
 
 export type AppRouter = typeof appRouter
