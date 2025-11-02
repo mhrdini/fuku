2c5b66debc3fafe721c08972e53988432b9bb680
Dini Putri <mahardini.rizky@gmail.com>
2025-11-02 11:08:44 +0900

    feat(ds): use shadcn for ui components, complete fk-22

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,10 +1,19 @@
+import { Suspense } from 'react'
+import { redirect } from 'next/navigation'
+
+import { getSession } from '~/auth/server'
 import { UserAuthForm } from '~/components/auth/user-auth-form'
 
 export default async function LoginPage() {
+  const session = await getSession()
+
+  if (session) {
+    redirect('/')
+  }
+
   return (
-    <div>
-      Login Page
+    <Suspense>
       <UserAuthForm />
-    </div>
+    </Suspense>
   )
 }
