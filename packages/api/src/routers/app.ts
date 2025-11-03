992bbee7e982d125d886fecf8b22ced7d7bbc13d
Dini Putri <mahardini.rizky@gmail.com>
2025-11-03 14:44:55 +0900

    refactor(db): refine team model, prepare for team feature implementation

--- packages/api/src/routers/app.ts
+++ packages/api/src/routers/app.ts
@@ -1,20 +1,12 @@
 import { createTRPCRouter } from '../trpc'
 import { authRouter } from './auth'
-import { locationRouter } from './schedule/location'
-import { shiftTypeRouter } from './schedule/shiftType'
 import { teamRouter } from './team'
-import { teamMemberRouter } from './team/member'
-import { payGradeRouter } from './team/payGrade'
 import { userRouter } from './user'
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   user: userRouter,
   team: teamRouter,
-  teamMember: teamMemberRouter,
-  payGrade: payGradeRouter,
-  location: locationRouter,
-  shiftType: shiftTypeRouter,
 })
 
 export type AppRouter = typeof appRouter
