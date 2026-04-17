75638086c3ead281c4db15564b113d89f2590a54
Dini Putri <mahardini.rizky@gmail.com>
2026-04-18 00:54:24 +0900

    feat: generate country consts with i18n-iso-countries for domain/i18n and nager api for infra/holiday

---
+++ packages/infrastructure/src/holiday/supported-countries.generated.ts
@@ -0,0 +1,372 @@
+// AUTO-GENERATED FILE — DO NOT EDIT
+
+export const SUPPORTED_COUNTRY_CODES = [
+  {
+    code: 'AD',
+  },
+  {
+    code: 'AL',
+  },
+  {
+    code: 'AM',
+  },
+  {
+    code: 'AR',
+  },
+  {
+    code: 'AT',
+  },
+  {
+    code: 'AU',
+  },
+  {
+    code: 'AX',
+  },
+  {
+    code: 'BA',
+  },
+  {
+    code: 'BB',
+  },
+  {
+    code: 'BD',
+  },
+  {
+    code: 'BE',
+  },
+  {
+    code: 'BG',
+  },
+  {
+    code: 'BJ',
+  },
+  {
+    code: 'BO',
+  },
+  {
+    code: 'BR',
+  },
+  {
+    code: 'BS',
+  },
+  {
+    code: 'BW',
+  },
+  {
+    code: 'BY',
+  },
+  {
+    code: 'BZ',
+  },
+  {
+    code: 'CA',
+  },
+  {
+    code: 'CD',
+  },
+  {
+    code: 'CG',
+  },
+  {
+    code: 'CH',
+  },
+  {
+    code: 'CL',
+  },
+  {
+    code: 'CN',
+  },
+  {
+    code: 'CO',
+  },
+  {
+    code: 'CR',
+  },
+  {
+    code: 'CU',
+  },
+  {
+    code: 'CY',
+  },
+  {
+    code: 'CZ',
+  },
+  {
+    code: 'DE',
+  },
+  {
+    code: 'DK',
+  },
+  {
+    code: 'DO',
+  },
+  {
+    code: 'EC',
+  },
+  {
+    code: 'EE',
+  },
+  {
+    code: 'EG',
+  },
+  {
+    code: 'ES',
+  },
+  {
+    code: 'FI',
+  },
+  {
+    code: 'FO',
+  },
+  {
+    code: 'FR',
+  },
+  {
+    code: 'GA',
+  },
+  {
+    code: 'GB',
+  },
+  {
+    code: 'GD',
+  },
+  {
+    code: 'GE',
+  },
+  {
+    code: 'GG',
+  },
+  {
+    code: 'GH',
+  },
+  {
+    code: 'GI',
+  },
+  {
+    code: 'GL',
+  },
+  {
+    code: 'GM',
+  },
+  {
+    code: 'GR',
+  },
+  {
+    code: 'GT',
+  },
+  {
+    code: 'GY',
+  },
+  {
+    code: 'HK',
+  },
+  {
+    code: 'HN',
+  },
+  {
+    code: 'HR',
+  },
+  {
+    code: 'HT',
+  },
+  {
+    code: 'HU',
+  },
+  {
+    code: 'ID',
+  },
+  {
+    code: 'IE',
+  },
+  {
+    code: 'IM',
+  },
+  {
+    code: 'IS',
+  },
+  {
+    code: 'IT',
+  },
+  {
+    code: 'JE',
+  },
+  {
+    code: 'JM',
+  },
+  {
+    code: 'JP',
+  },
+  {
+    code: 'KE',
+  },
+  {
+    code: 'KR',
+  },
+  {
+    code: 'KZ',
+  },
+  {
+    code: 'LI',
+  },
+  {
+    code: 'LS',
+  },
+  {
+    code: 'LT',
+  },
+  {
+    code: 'LU',
+  },
+  {
+    code: 'LV',
+  },
+  {
+    code: 'MA',
+  },
+  {
+    code: 'MC',
+  },
+  {
+    code: 'MD',
+  },
+  {
+    code: 'ME',
+  },
+  {
+    code: 'MG',
+  },
+  {
+    code: 'MK',
+  },
+  {
+    code: 'MN',
+  },
+  {
+    code: 'MS',
+  },
+  {
+    code: 'MT',
+  },
+  {
+    code: 'MX',
+  },
+  {
+    code: 'MZ',
+  },
+  {
+    code: 'NA',
+  },
+  {
+    code: 'NE',
+  },
+  {
+    code: 'NG',
+  },
+  {
+    code: 'NI',
+  },
+  {
+    code: 'NL',
+  },
+  {
+    code: 'NO',
+  },
+  {
+    code: 'NZ',
+  },
+  {
+    code: 'PA',
+  },
+  {
+    code: 'PE',
+  },
+  {
+    code: 'PG',
+  },
+  {
+    code: 'PH',
+  },
+  {
+    code: 'PL',
+  },
+  {
+    code: 'PR',
+  },
+  {
+    code: 'PT',
+  },
+  {
+    code: 'PY',
+  },
+  {
+    code: 'RO',
+  },
+  {
+    code: 'RS',
+  },
+  {
+    code: 'RU',
+  },
+  {
+    code: 'SC',
+  },
+  {
+    code: 'SE',
+  },
+  {
+    code: 'SG',
+  },
+  {
+    code: 'SI',
+  },
+  {
+    code: 'SJ',
+  },
+  {
+    code: 'SK',
+  },
+  {
+    code: 'SM',
+  },
+  {
+    code: 'SR',
+  },
+  {
+    code: 'SV',
+  },
+  {
+    code: 'TN',
+  },
+  {
+    code: 'TR',
+  },
+  {
+    code: 'UA',
+  },
+  {
+    code: 'UG',
+  },
+  {
+    code: 'US',
+  },
+  {
+    code: 'UY',
+  },
+  {
+    code: 'VA',
+  },
+  {
+    code: 'VE',
+  },
+  {
+    code: 'VN',
+  },
+  {
+    code: 'ZA',
+  },
+  {
+    code: 'ZW',
+  },
+] as const
+
+export const SUPPORTED_COUNTRY_SET = new Set(SUPPORTED_COUNTRY_CODES)
